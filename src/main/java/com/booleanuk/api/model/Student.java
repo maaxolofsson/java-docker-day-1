e0d85a9e58cb4beaa3671ff868ea87729411ce53
Max Olofsson <max.olofsson@se.experis.com>
2024-09-18 12:56:16 +0200

    implemented student model

---
+++ src/main/java/com/booleanuk/api/model/Student.java
@@ -0,0 +1,43 @@
+package com.booleanuk.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "students")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String dob;
+
+    @Column
+    private String courseTitle;
+
+    @Column
+    private String courseStartDate;
+
+    @Column
+    private double averageGrade;
+
+    public Student(int id) {
+        this.id = id;
+    }
+    
+}
